2d826ab9570ab6307e6adde5d337fc1bff41af70
Bonnyfication <philipp.bonny@googlemail.com>
2022-12-27 12:09:47 +0100

    docs - change link crawler repo

--- packages/sloth_link_crawler/example/sloth_link_crawler_example.dart
+++ packages/sloth_link_crawler/example/sloth_link_crawler_example.dart
@@ -1,6 +1,2 @@
-import 'package:sloth_link_crawler/sloth_link_crawler.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+// TODO: Add example
+void main() {}
